0f09d2276f81d494fb49d5b91c0eec9fab2def90
Lauren Benichou <laurenbenichou@gmail.com>
2014-11-20 11:38:44 -0800

    improving mobile view

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $( function() {
   });
 if (Modernizr.touch) {
   $(".overlay").addClass("overlay-no-touch")
+   $(".overlay-blog").addClass("overlay-blog-no-touch")
   $("h3").addClass("h3-no-touch")
 }
 })

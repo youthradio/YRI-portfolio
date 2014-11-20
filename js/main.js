37199f6ab1611c268138f957515e7d2e26548786
Lauren Benichou <laurenbenichou@gmail.com>
2014-11-20 12:09:58 -0800

    added imagesLoaded.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $( function() {
-  // init Isotope
-  var $container = $('#isotope').isotope({
+  // init Isotope when images are loaded.
+  var $container = $('#isotope').imagesLoaded(function() {
+    $container.isotope({
     itemSelector: '.item',
     layoutMode: 'masonry',
     masonry: {
       gutter: 10
     }
   });
+});
+
 if (Modernizr.touch) {
   $(".overlay").addClass("overlay-no-touch")
    $(".overlay-blog").addClass("overlay-blog-no-touch")
